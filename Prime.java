698abad15dc1dada1a85b6f47d6095c8fc7a96f9
cvk09 <chejaralavinodkumar@gmail.com>
2021-09-14 18:34:39 +0530

    first commit

--- Prime.java
+++ Prime.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Prime {
 	  public static void main(String args[])
 	    {
-	        int num, i, count=0;
+	        int num, i, count=2;
 	        Scanner scan = new Scanner(System.in);
 	      
 	        System.out.print("Enter a Number : ");
